ea554fab50d1a8a163ac106be2025024a1954b9d
tseng-chiao <115610077+tseng-chiao@users.noreply.github.com>
2023-01-10 14:27:09 +0800

    Delete inc directory

--- inc/file.h
+++
@@ -1,9 +0,0 @@
-#ifndef __FILE_H__
-#define __FILE_H__
-
-//#include "movechess.h"
-
-void Push(int startcoor1, int startcoor2, int startsymb, int endcoor1, int endcoor2, int endsymb);
-int Pop();
-
-#endif
